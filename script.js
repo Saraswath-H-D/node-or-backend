90bec092ee40a3444d82074b9545ea64d488e416
student <saraswathhd2@gmail.com>
2026-03-20 00:35:11 +0530

    hello

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let n=5;
 for(let i=1;i<=n;i++){
     console.log("be" + i);//we run in git bash by typing node script.js
-    
 }
+let arg=process.argv;
+for(let i=0;i<arg.length;i++){
+    console.log("hello,rbr" + arg[i]);
+}
+console.log(process.argv)//returns when we run in git bash by typing node script.js hello world
