091e2fa692f4889cea82707c37ae149da660dd0d
student <saraswathhd2@gmail.com>
2026-04-25 19:11:05 +0530

    mysql2

--- SQLCLASS/index.js
+++ SQLCLASS/index.js
@@ -1,5 +1,37 @@
 const { faker } = require('@faker-js/faker');
-
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password:'Sara@my26'
+});
+try{
+connection.query("show Tables",(err,result)=>{
+   if(err)throw err;
+   console.log(result);
+});
+}catch(err){
+  console.log(err);
+}
+ const [results] = await connection.query(
+    'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',//? to indicate values to insert
+    ['Page', 45]
+  );
+  let data=[];
+  for(let i=1;i<=100;i++){
+    data.push(getRandomUser());//for bulk insertion 
+  }
+  app.get("/",(req,res)=>{
+    let q=`select count(*) from user`;
+    try{
+      connection.query(q,(err,result)=>{
+   if(err)throw err;
+   console.log(result);//for routing in db
+   res.send(result)
+});
+    }
+  })
 let getRandomUser=()=> { //faker is used to generate fake detailed datas for easy understanding
   return {
     userId: faker.string.uuid(),
@@ -8,4 +40,4 @@ let getRandomUser=()=> { //faker is used to generate fake detailed datas for eas
     password: faker.internet.password()
   };
 };
-console.log(getRandomUser())
+// console.log(getRandomUser())
